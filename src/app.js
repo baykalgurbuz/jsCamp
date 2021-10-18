5160aafe4a1078747c9ac64e962c4a62a3e603e5
Enes Baykal Gürbüz <ebaykalgurbuz@gmail.com>
2021-10-19 02:09:44 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("Merhaba")
+
+let dolarDun = 9.20
+let dolarBugun=10.20
+
+console.log(dolarBugun)
+
+let konutKredisi =["Konut Kredisi","Emlak KOnut Kredisi","Araba kredisi",["a","b"]]
+for(let i;i<konutKredisi.length;i++){
+    console.log(konutKredisi[i])
+}
+console.log(konutKredisi[3][1])
